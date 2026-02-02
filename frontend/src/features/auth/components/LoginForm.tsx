4b11ee91d6e3f6cfac544c592270c82e7bab9ac2
rnapyzz <96988008+rnapyzz@users.noreply.github.com>
2026-02-02 15:52:07 +0900

    feat:ログインページをリファクタリング

--- frontend/src/features/auth/components/LoginForm.tsx
+++ frontend/src/features/auth/components/LoginForm.tsx
@@ -1,52 +1,62 @@
-import {
-    Card,
-    CardContent,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card.tsx";
+import { Card, CardContent } from "@/components/ui/card.tsx";
 import { Button } from "@/components/ui/button.tsx";
 import { Label } from "@/components/ui/label.tsx";
 import { Input } from "@/components/ui/input.tsx";
-import * as React from "react";
+import { type SyntheticEvent, useState } from "react";
 
 export function LoginForm() {
-    const handleSubmit = (e: React.SubmitEvent) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e: SyntheticEvent) => {
         e.preventDefault();
-        console.log(e.target.value);
+        console.log("入力された値: ", { email, password });
     };
     return (
         <Card className="rounded-md w-[380px]">
             <CardContent>
                 <form onSubmit={handleSubmit} className="py-6">
-                    <div className="py-4 space-y-3">
+                    <div className="py-6  space-y-3">
                         <div>
                             <Label className="text-slate-900">
                                 メールアドレス
                             </Label>
-                            <Input id="email" name="email"></Input>
+                            <Input
+                                type="email"
+                                id="email"
+                                name="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
                         </div>
                         <div>
                             <Label className="text-slate-900">パスワード</Label>
-                            <Input id="password" name="password"></Input>
+                            <Input
+                                type="password"
+                                id="password"
+                                name="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                         </div>
                     </div>
+                    <Button
+                        type="submit"
+                        className="w-full py-6 rounded-sm bg-slate-800 text-white"
+                    >
+                        ログイン
+                    </Button>
+                    <p className="text-xs text-slate-700 pt-4">
+                        続行することでGhostの利用規約に同意し、Ghostのプライバシーポリシーを確認したものとみなされます。
+                    </p>
                 </form>
-                <div className="flex justify-center items-center mb-4">
+                <div className="flex justify-center items-center">
                     <a className="text-xs text-sky-700">パスワードを忘れた方</a>
                     <span className="text-xs text-slate-400 px-2">/</span>
                     <a className="text-xs text-sky-700">
                         ログインでお困りの場合
                     </a>
                 </div>
-                <Button
-                    type="submit"
-                    className="w-full py-6 rounded-sm bg-slate-800 text-white"
-                >
-                    ログイン
-                </Button>
-                <p className="text-xs text-slate-700 py-4">
-                    続行することでGhostの利用規約に同意し、Ghostのプライバシーポリシーを確認したものとみなされます。
-                </p>
             </CardContent>
             <div className="flex justify-between items-center p-6 border-t">
                 <p className="text-sm text-slate-900">
